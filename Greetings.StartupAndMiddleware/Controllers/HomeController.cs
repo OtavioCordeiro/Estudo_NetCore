0f6cce3c8ea576037d6c8cc2c5c81ac2243fcc15
Otavio Cordeiro Lopes <olopes@braspag.com.br>
2019-02-04 16:01:13 -0200

    Adiciona Model

--- Greetings.StartupAndMiddleware/Controllers/HomeController.cs
+++ Greetings.StartupAndMiddleware/Controllers/HomeController.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Greetings.StartupAndMiddleware.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Greetings.StartupAndMiddleware.Controllers
 {
     [Route("api")]
     public class HomeController : Controller
     {
+        private IGreeting _greeting;
+
+        public HomeController(IGreeting greeting)
+        {
+            _greeting = greeting;
+        }
+
         public IActionResult Index()
         {
-            return View("Home");
+            GreetingModel model = new GreetingModel()
+            {
+                CreateDate = DateTime.Now,
+                Id = 1,
+                Message = _greeting.GetMessageOfDay()
+            };
+
+            return View(model);
         }
     }
 }
